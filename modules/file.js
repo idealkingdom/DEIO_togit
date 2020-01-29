48a19a5c3fcaef0beab4d52d61678f4af52c13ef
Jeff <jefferic506@gmail.com>
2020-01-29 16:37:36 +0800

    Git add API

---
+++ modules/file.js
@@ -0,0 +1,36 @@
+const axios = require('axios')
+const Path = require('path')  
+const fs = require('fs')
+
+async function downloadF(link,filename) {
+		const path = Path.resolve(__dirname, 'Files', filename)
+		response = await axios.get(`http://survey-d.dynata.com/api/v1/surveys/selfserve/${link}/files/${filename}`,{
+		headers: {'x-apikey': '0m640xsrvkpp41gq1hsq8bqvg9wewd4f7naj3w2rsv7vn5cxfqd7tmuxrgabcy4w',
+				  'Content-Type':'application/octet-stream;'},
+				  responseType: 'blob'
+				
+			})
+		getdl = await function(e) {
+		  const url = window.URL.createObjectURL(new Blob([e],{ type: 'application/octet-stream;'}));
+  		  const link1 = document.createElement('a');
+  		  link1.href = url;
+  		  link1.setAttribute('download', filename.toString());
+  		  document.body.appendChild(link1);
+  		  link1.click();
+		};
+
+		getdl(response.data)
+}
+
+async function getFiles(link){
+	const response = await axios.get(`http://survey-d.dynata.com/api/v1/surveys/selfserve/${link}/files`,{
+				headers: {'x-apikey': '0m640xsrvkpp41gq1hsq8bqvg9wewd4f7naj3w2rsv7vn5cxfqd7tmuxrgabcy4w',
+						  'Content-Type': 'text/plain',
+				          'responseType': 'stream'
+				}
+			})
+	console.log(response.data)
+
+}
+
+downloadF('53b/1909631','survey.xml')
